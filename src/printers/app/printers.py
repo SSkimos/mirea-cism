3d16d88098fc344afe1fa97eaa1c189e2018b5db
Chereshnev <chereshnev.n.s@edu.mirea.ru>
2022-11-11 15:25:51 +0300

    :wrench: krakend in progress

--- src/printers/app/printers.py
+++ src/printers/app/printers.py
@@ -3,7 +3,10 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-
 @app.get("/")
+def basic():
+    return {"Hello": "documents"}
+
+@app.get("/printers/")
 def read_root():
     return {"Hello": "printers"}
